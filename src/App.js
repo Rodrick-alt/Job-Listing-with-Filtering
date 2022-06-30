a237db5fbb5b4e7b928511c5784802e9998645aa
Rodrick C <Rodrickmc42@gmail.com>
2022-06-30 15:07:26 -0500

    listings component display json data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import JsonData from './data.json';
 
+
+
 function App() {
   const [array, setArray] = useState([]);
 
   function SearchBar(tab) {
-    if (tab !== '' && tab !== 'Clear') {
+    if (tab !== '' && array.includes('HTML') === false) {
+      console.log(array.includes('HTML'));
       let filterTab = <button key={Math.random(0, 201)}>{tab}<p>X</p></button>;
       setArray(old => [old, filterTab]);
     }
@@ -18,6 +21,7 @@ function App() {
     //   setArray(old=>[old,filterTab])
     // }, filterTab);
   }
+
   function HandleClear() {
     setArray(old => []);
     document.getElementById('search-bar').style.display = 'none';
@@ -30,36 +34,46 @@ function App() {
 
   function FillListings() {
     const [listings, setListings] = useState([]);
-
-    return (
-      <div className='job-post'>
+    let mapped = JsonData.slice().map(data =>
+      <div className='job-post featured-job new-job'>
 
         <div className='group1'>
           <img className='company-img' src={require('./images/photosnap.svg').default} alt='logo' />
 
           <div className='job-details'>
             <div className='company-name'>
-              Company Name
+              {data.company}
               <div className='new-tag'>NEW!</div>
               <div className='featured-tag'>FEATURED</div>
             </div>
 
-            <p className='job-position'>Position</p>
+            <p className='job-position'>{data.position}</p>
             <div className='job-type'>
-              <p>Date</p>
+              <p>{data.postedAt}</p>
               &#8226;
-              <p>Job Type</p>
+              <p>{data.contract}</p>
               &#8226;
-              <p>Location</p>
+              <p>{data.location}</p>
             </div>
           </div>
 
         </div>
 
         <div className='filter-tablets'>
-          <button onClick={() => HandleFillBar('HTML')}>HTML</button>
+          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
         </div>
-      </div>);
+      </div>
+    )
+
+    useEffect(() => {
+      setListings(old => [...mapped])
+    }, []);
+
+    return (listings)
   }
 
   return (
@@ -84,3 +98,36 @@ function App() {
 }
 
 export default App;
+
+      // <div className='job-post featured-job new-job'>
+
+      //   <div className='group1'>
+      //     <img className='company-img' src={require('./images/photosnap.svg').default} alt='logo' />
+
+      //     <div className='job-details'>
+      //       <div className='company-name'>
+      //         Company Name
+      //         <div className='new-tag'>NEW!</div>
+      //         <div className='featured-tag'>FEATURED</div>
+      //       </div>
+
+      //       <p className='job-position'>Position</p>
+      //       <div className='job-type'>
+      //         <p>Date</p>
+      //         &#8226;
+      //         <p>Job Type</p>
+      //         &#8226;
+      //         <p>Location</p>
+      //       </div>
+      //     </div>
+
+      //   </div>
+
+      //   <div className='filter-tablets'>
+      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
+      //   </div>
+      // </div>;
