62163d08049426891eefcddd7e6dd912a7e27511
Rodrick C <Rodrickmc42@gmail.com>
2022-07-01 15:02:38 -0500

    fixed issue causing unnecessary rerenders, and refactored app-js code.

--- src/App.js
+++ src/App.js
@@ -5,76 +5,90 @@ import { useEffect } from 'react';
 import JsonData from './data.json';
 
 
-
 function App() {
+  const [listings, setListings] = useState([]);
   const [array, setArray] = useState([]);
+
+  // Setter Functions called in their own components have to be conditional. OR-
+  // Stops infinite re-renders by only excuting setter functions in useEffect.
+  window.onload = () => {
+    setListings(old => FillListings);
+  }
+
+  function FillListings() {
+    let mapped = JsonData.slice().map(data => {
+      let arr = [];
+      arr.push(<button onClick={() => HandleFillBar(data.role)}>{data.role}</button>);
+      arr.push(<button onClick={() => HandleFillBar(data.level)}>{data.level}</button>);
+      arr.push(data.languages.slice().map(language => {
+        return (
+          <button onClick={() => HandleFillBar(language)}>{language}</button>
+        )
+      })
+      );
+      if (data.tools !== []) {
+        arr.push(data.tools.slice().map(tool => {
+          return (
+            <button onClick={() => HandleFillBar(tool)}>{tool}</button>
+          )
+        })
+        )
+      };
+
+      return (
+        <div className='job-post featured-job new-job'>
+          <div className='group1'>
+            <img className='company-img' src={data.logo} alt='logo' />
+
+            <div className='job-details'>
+              <div className='company-name'>
+                {data.company}
+                <div className='new-tag'>NEW!</div>
+                <div className='featured-tag'>FEATURED</div>
+              </div>
+
+              <p className='job-position'>{data.position}</p>
+              <div className='job-type'>
+                <p>{data.postedAt}</p>
+                &#8226;
+                <p>{data.contract}</p>
+                &#8226;
+                <p>{data.location}</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='filter-tablets'>
+            {arr}
+          </div>
+        </div>
+      )
+    }
+    )
+    return ([...mapped])
+  }
+
 
   function SearchBar(tab) {
     if (tab !== '' && array.includes('HTML') === false) {
       console.log(array.includes('HTML'));
-      let filterTab = <button key={Math.random(0, 201)}>{tab}<p>X</p></button>;
-      setArray(old => [old, filterTab]);
+      setArray(old => [old, <button key={Math.random(0, 201)}>{tab}<p>X</p></button>])
     }
-    // Stops infinite re-renders by only excuting useEffect when The value(Mapped) changes.
-    // Setter Functions called in their own components have to be conditional. 
-    // useEffect(() => {
-    //   setArray(old=>[old,filterTab])
-    // }, filterTab);
   }
 
   function HandleClear() {
     setArray(old => []);
     document.getElementById('search-bar').style.display = 'none';
   }
-
   function HandleFillBar(tab) {
-    SearchBar(tab)
-    document.getElementById('search-bar').style.display = 'flex';
+    SearchBar(tab);
+    let bar = document.getElementById('search-bar');
+    if (bar.style.display === 'none') {
+      bar.style.display = 'flex';
+    }
   }
 
-  function FillListings() {
-    const [listings, setListings] = useState([]);
-    let mapped = JsonData.slice().map(data =>
-      <div className='job-post featured-job new-job'>
-
-        <div className='group1'>
-          <img className='company-img' src={require('./images/photosnap.svg').default} alt='logo' />
-
-          <div className='job-details'>
-            <div className='company-name'>
-              {data.company}
-              <div className='new-tag'>NEW!</div>
-              <div className='featured-tag'>FEATURED</div>
-            </div>
-
-            <p className='job-position'>{data.position}</p>
-            <div className='job-type'>
-              <p>{data.postedAt}</p>
-              &#8226;
-              <p>{data.contract}</p>
-              &#8226;
-              <p>{data.location}</p>
-            </div>
-          </div>
-
-        </div>
-
-        <div className='filter-tablets'>
-          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-          <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-        </div>
-      </div>
-    )
-
-    useEffect(() => {
-      setListings(old => [...mapped])
-    }, []);
-
-    return (listings)
-  }
 
   return (
     <div id='page-wrapper'>
@@ -82,7 +96,6 @@ function App() {
       </header>
 
       <main>
-
         <div id='search-bar'>
           <div className='search-tablets'>
             {array}
@@ -90,7 +103,6 @@ function App() {
           <button id='clear-btn'
             onClick={() => HandleClear()}>Clear</button>
         </div>
-
         <FillListings />
       </main>
     </div>
@@ -98,36 +110,3 @@ function App() {
 }
 
 export default App;
-
-      // <div className='job-post featured-job new-job'>
-
-      //   <div className='group1'>
-      //     <img className='company-img' src={require('./images/photosnap.svg').default} alt='logo' />
-
-      //     <div className='job-details'>
-      //       <div className='company-name'>
-      //         Company Name
-      //         <div className='new-tag'>NEW!</div>
-      //         <div className='featured-tag'>FEATURED</div>
-      //       </div>
-
-      //       <p className='job-position'>Position</p>
-      //       <div className='job-type'>
-      //         <p>Date</p>
-      //         &#8226;
-      //         <p>Job Type</p>
-      //         &#8226;
-      //         <p>Location</p>
-      //       </div>
-      //     </div>
-
-      //   </div>
-
-      //   <div className='filter-tablets'>
-      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-      //     <button onClick={() => HandleFillBar('HTML')}>JavaScript</button>
-      //   </div>
-      // </div>;
