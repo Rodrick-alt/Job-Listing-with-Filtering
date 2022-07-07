a6d4e8255745e275078b619892831f43ecf073fa
Rodrick C <Rodrickmc42@gmail.com>
2022-07-07 13:08:27 -0500

    added listings filtering by search bar tablets

--- src/App.js
+++ src/App.js
@@ -4,50 +4,71 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import JsonData from './data.json';
 
-//added new and featured job differiante
 
 function App() {
   // job listing array
   const [listings, setListings] = useState([]);
   // search array
   const [searchArray, setSearchArray] = useState([]);
-  let checkArr = [];
+  const [checkArr, setCheckArr] = useState([]);
 
-  // Setter Functions called in their own components have to be conditional. OR-
-  // Stops infinite re-renders by only excuting setter functions in useEffect.
-  window.onload = () => {
-    setListings(old => <FillListings />);
+
+  function handleClear() {
+    setCheckArr(old => []);
+    setSearchArray(old => []);
+    document.getElementById('search-bar').style.display = 'none';
+  }
+
+  function removeSearchBtn(index) {
+    setCheckArr(old => {
+      old.splice(index, 1);
+      return [...old]
+    })
+    setSearchArray(old => {
+      old.splice(index, 1);
+      if (old.length === 0) {
+        document.getElementById('search-bar').style.display = 'none';
+      }
+      return [...old]
+    })
+  }
+
+  function searchBar(tab) {
+    //do all your functionality in setter functions
+    //so you have access to the updated variables
+    setCheckArr(old => {
+      if (old.includes(tab) === false) {
+        //didn't know you can nest setter functions, neat!
+        setSearchArray(old => [...old, <button key={old.length}
+          onClick={() => removeSearchBtn(old.length)}>{tab}<p>X</p></button>]);
+        document.getElementById('search-bar').style.display = 'flex';
+        return [...old, tab]
+      } else { return [...old] }
+    });
   }
 
   function FillListings() {
     let mapped = JsonData.slice().map(data => {
       // job filter tablets
       let arr = [];
-      arr.push(<button onClick={() => HandleFillBar(data.role)}>{data.role}</button>);
-      arr.push(<button onClick={() => HandleFillBar(data.level)}>{data.level}</button>);
-      arr.push(data.languages.slice().map(language => {
-        return (
-          <button onClick={() => HandleFillBar(language)}>{language}</button>
-        )
-      })
-      );
+      arr.push(<button onClick={() => searchBar(data.role)}>{data.role}</button>);
+      arr.push(<button onClick={() => searchBar(data.level)}>{data.level}</button>);
+      arr.push(data.languages.slice().map(language => <button onClick={() =>
+        searchBar(language)}>{language}</button>));
+
       if (data.tools !== []) {
-        arr.push(data.tools.slice().map(tool => {
-          return (
-            <button onClick={() => HandleFillBar(tool)}>{tool}</button>
-          )
-        })
-        )
+        arr.push(data.tools.slice().map(tool => <button onClick={() =>
+          searchBar(tool)}>{tool}</button>));
       };
 
       // job post styling
-      let styling = 'job-post'
+      let styling = 'job-post';
       if (data.featured)
-        styling = 'job-post featured-job'
+        styling = 'job-post featured-job';
       if (data.new)
-        styling = 'job-post new-job'
+        styling = 'job-post new-job';
       if (data.featured && data.new)
-        styling = 'job-post featured-job new-job'
+        styling = 'job-post featured-job new-job';
 
       return (
         <div className={styling}>
@@ -70,7 +91,6 @@ function App() {
                 <p>{data.location}</p>
               </div>
             </div>
-
           </div>
 
           <div className='filter-tablets'>
@@ -80,43 +100,80 @@ function App() {
       )
     }
     )
-    // console.log(mapped[0])
     return (mapped)
   }
 
+  // Restructuring Listings whenenver filtertabs are added to searchbar
+  useEffect(() => {
+    let mapped = JsonData.slice().map(data => {
+      // job filter tablets
+      let arr = [];
+      arr.push(<button onClick={() => searchBar(data.role)}>{data.role}</button>);
+      arr.push(<button onClick={() => searchBar(data.level)}>{data.level}</button>);
+      arr.push(data.languages.slice().map(language => <button onClick={() =>
+        searchBar(language)}>{language}</button>));
 
-  function HandleClear() {
-    checkArr = [];
-    setSearchArray(old => []);
-    document.getElementById('search-bar').style.display = 'none';
+      if (data.tools !== []) {
+        arr.push(data.tools.slice().map(tool => <button onClick={() =>
+          searchBar(tool)}>{tool}</button>));
+      };
+
+      // job post styling
+      let styling = 'job-post';
+      if (data.featured)
+        styling = 'job-post featured-job';
+      if (data.new)
+        styling = 'job-post new-job';
+      if (data.featured && data.new)
+        styling = 'job-post featured-job new-job';
+
+      // job-post to searchbar filter-tabs comparison
+      let jobFilterTabs = [data.role, data.level, ...data.languages.slice(), ...data.tools.slice()];
+      const checking = (item) => jobFilterTabs.includes(item);
+      if (!checkArr.every(checking)) {
+        styling = 'job-post-off'
+      }
+
+      return (
+        <div className={styling}>
+          <div className='group1'>
+            <img className='company-img' src={data.logo} alt='logo' />
+
+            <div className='job-details'>
+              <div className='company-name'>
+                {data.company}
+                <div className='new-tag'>NEW!</div>
+                <div className='featured-tag'>FEATURED</div>
+              </div>
+
+              <p className='job-position'>{data.position}</p>
+              <div className='job-type'>
+                <p>{data.postedAt}</p>
+                &#8226;
+                <p>{data.contract}</p>
+                &#8226;
+                <p>{data.location}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='filter-tablets'>
+            {arr}
+          </div>
+        </div>
+      )
+    }
+    )
+    setListings(old => [...mapped])
+  }, [searchArray, checkArr])
+
+
+
+  // Setter Functions called in their own components have to be conditional. OR-
+  // Stop infinite re-renders by only excuting setter functions with useEffect in a component.
+  window.onload = () => {
+    setListings(old => <FillListings />);
   }
-
-  function SearchBar(tab) {
-    if (checkArr.includes(tab) == false) {
-      checkArr.push(tab);
-      setSearchArray(old => [...old, <button key={old.length}
-        onClick={() => RemoveSearchBtn(old.length)}>{tab}<p>X</p></button>]);
-      document.getElementById('search-bar').style.display = 'flex';
-    } else { console.log(checkArr) }
-  }
-
-  function HandleFillBar(tab) {
-    SearchBar(tab);
-  }
-
-
-  function RemoveSearchBtn(index) {
-    console.log(index)
-    // index -= 1;
-    // if (index < 0) index = 1;
-    // checkArr.splice(index, 1);
-    // setSearchArray(old => {
-    //   old.splice(index, 1);
-    //   return [...old]
-    // })
-  }
-
-
 
   return (
     <div id='page-wrapper'>
@@ -129,7 +186,7 @@ function App() {
             {searchArray}
           </div>
           <button id='clear-btn'
-            onClick={HandleClear}>Clear</button>
+            onClick={() => handleClear()}>Clear</button>
         </div>
         {listings}
       </main>
